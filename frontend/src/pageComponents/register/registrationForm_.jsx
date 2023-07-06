50995143784ee2ceb28d8537a9273bcd52039928
hidden hero <hiddenhero47pro@gmail.com>
2023-07-06 13:18:03 +0100

    Minor Changes

---
+++ frontend/src/pageComponents/register/registrationForm_.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MyForm } from "./registrationForm.style";
+
+function RegistrationForm() {
+  return (
+    <MyForm>
+        <div>RegistrationForm</div>
+    </MyForm>
+  )
+}
+
+export default RegistrationForm;
